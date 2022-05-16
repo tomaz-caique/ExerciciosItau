69af5b1b2164fc00c46b179074ad47b10162f818
tomaz-caique <56745593+tomaz-caique@users.noreply.github.com>
2022-05-15 22:44:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Cepom
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecutarContasAPagar_ISS_CPOM(
+                false, 
+                false, 
+                false, 
+                false, 
+                false);    
+                
+            ExecutarContasAPagar_ISS_CPOM(
+                true, 
+                false, 
+                false, 
+                false, 
+                false);
+                
+            ExecutarContasAPagar_ISS_CPOM(
+                true, 
+                true, 
+                true, 
+                true, 
+                false);
+                
+            ExecutarContasAPagar_ISS_CPOM(
+                true, 
+                false, 
+                true, 
+                true, 
+                false);
+        }
+        
+        static void ExecutarContasAPagar_ISS_CPOM(
+            bool naturezaCalculaISS, 
+            bool fornecedorRecolheISS, 
+            bool fornecedorDeOutroMunicipio, 
+            bool municipioDoTomadorExigeCPOM, 
+            bool possuiCPOM)
+        {
+            Console.Write($"> naturezaCalculaISS: {naturezaCalculaISS} ");
+            Console.Write($"fornecedorRecolheISS: {fornecedorRecolheISS} ");
+            Console.Write($"fornecedorDeOutroMunicipio: {fornecedorDeOutroMunicipio} ");
+            Console.Write($"municipioDoTomadorExigeCPOM: {municipioDoTomadorExigeCPOM} ");
+            Console.WriteLine($"possuiCPOM: {possuiCPOM} ");      
+            
+            //Início do trecho para alteração
+
+            Iniciar();
+           IncluirContasAPagar();
+
+            if (naturezaCalculaISS == true && fornecedorRecolheISS == false)
+                TributarPrestadorDeServicos();
+            if (naturezaCalculaISS == true && fornecedorRecolheISS == true) 
+                TributarTomadorDeServicos();
+            if (fornecedorRecolheISS == false && municipioDoTomadorExigeCPOM == true)
+               TributarTomadorDeServicos();
+
+            Finalizar();
+            //Fim do trecho para alteração
+            
+            Console.WriteLine();
+        }
+        
+        static void IncluirContasAPagar()
+        {
+            Console.WriteLine("IncluirContasAPagar");
+        }
+        
+        static void TributarPrestadorDeServicos()
+        {
+            Console.WriteLine("TributarPrestadorDeServicos");
+        }
+        
+        static void TributarTomadorDeServicos()
+        {
+            Console.WriteLine("TributarTomadorDeServicos");
+        }    
+        
+        static void Iniciar()
+        {
+            Console.WriteLine("Iniciar");
+        }
+        
+        static void Finalizar()
+        {
+            Console.WriteLine("Finalizar");
+        }    
+  
+    }
+}
